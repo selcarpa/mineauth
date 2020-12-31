a8371b7163e0e3de2da40954768ced8de7c5e16b
selcarpa@gmail.com <selcarpa@gmail.com>
2020-12-31 09:19:07 +0800

    fix register command can not resolve argument correctly

--- src/main/java/cn/aethli/mineauth/command/BaseCommand.java
+++ src/main/java/cn/aethli/mineauth/command/BaseCommand.java
@@ -4,23 +4,32 @@ import cn.aethli.mineauth.entity.BaseEntity;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 public abstract class BaseCommand<T extends BaseEntity> implements Command<CommandSource> {
   protected LiteralArgumentBuilder<CommandSource> builder;
 
-  public BaseCommand(String command,  List<String> parameters) {
-    this.builder =
-        Commands.literal(command);
-    if (parameters != null) {
+  public BaseCommand(String command, List<String> parameters) {
+    this.builder = Commands.literal(command);
+
+    if (null != parameters && !parameters.isEmpty()) {
+      final RequiredArgumentBuilder<CommandSource, String>[] argument =
+          new RequiredArgumentBuilder[] {
+            Commands.argument(parameters.get(0), StringArgumentType.string())
+          };
+      parameters.remove(0);
       parameters.forEach(
-          parameter -> builder.then(Commands.argument(parameter, StringArgumentType.string()).executes(this)));
+          parameter ->
+              argument[0] =
+                  argument[0].then(Commands.argument(parameter, StringArgumentType.string())));
+      argument[0].executes(this);
+    } else {
+      builder.executes(this);
     }
-    builder.executes(this);
   }
 
   public LiteralArgumentBuilder<CommandSource> getBuilder() {
