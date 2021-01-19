4f96b08834572c162ce67d529562cb8fccca9dbb
selcarpa@gmail.com <selcarpa@gmail.com>
2021-01-19 14:19:33 +0800

    update

---
+++ src/main/java/cn/aethli/mineauth/exception/DataRuntimeException.java
@@ -0,0 +1,9 @@
+package cn.aethli.mineauth.exception;
+
+public class DataRuntimeException extends RuntimeException {
+
+    public DataRuntimeException(String message) {
+        super(message);
+    }
+
+}
